91d3456124af466156fb8e45e2afe138e8a6f094
Rahul <basicfunc@gmail.com>
2022-02-22 15:06:14 +0530

    Added test to check if interpreter support inline_evaluation

--- tests/inline_eval.sh
+++ tests/inline_eval.sh
@@ -1,6 +1,12 @@
 echo "*************Inline Evaluation TEST****************"
 
-INLINE_EVAL="\"(+ 1 2 3 4 5 6 7 8 9 0)(- 1 2 3 4 5 6 7 8 9 0)(* 1 2 3 4 5 6 7 8 9 0)(/ 1 2 3 4 5 6 7 8 9 0)(print \"Inline Eval is WORKING!!\")\""
-echo $INLINE_EVAL
+INLINE_EVAL='""'
 
-${YIPPY_EXE} -e $INLINE_EVAL
+res=$(${YIPPY_EXE} -e $INLINE_EVAL)
+
+if [[ $? -ne 0 ]]
+then
+    echo -e "Failed. Make sure, you compiled the program correctly."
+else
+    echo -e "Working Fine."
+fi
