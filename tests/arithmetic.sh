ee2ebae2935d76e3a759a0f0a18d9c9c6c732030
Rahul <basicfunc@gmail.com>
2022-02-22 11:55:20 +0530

    Using pyhton instead of awk to test precisely

--- tests/arithmetic.sh
+++ tests/arithmetic.sh
@@ -4,75 +4,67 @@ var1=$RANDOM
 var2=$RANDOM
 
 echo "Addition Test"
-echo -e "Adding $var1 and $var2\n\n"
+echo "Adding $var1 and $var2"
 yippy_test=$(${YIPPY_EXE} -e "+ $var1 $var2")
 bash_test=$(expr ""$var1 "+" $var2"")
 
 if [[ $yippy_test -eq $bash_test ]]; then
-	echo "Test:OK"
+	echo -e "Test:OK\n"
 	count_ok=$((count_ok + 1))
 else
-	echo "Test:Fail"
+	echo -e "Test:Fail\n"
 	count_fail=$((count_fail + 1))
 fi
 
-echo -e "\n"
-
 echo "Subtraction Test"
-echo -e "Subtracting $var1 and $var2\n\n"
+echo "Subtracting $var1 and $var2"
 yippy_test=$(${YIPPY_EXE} -e "- $var1 $var2")
 bash_test=$(expr ""$var1 "-" $var2"")
 
 if [[ $yippy_test -eq $bash_test ]]; then
-	echo "Test:OK"
+	echo -e "Test:OK\n"
 	count_ok=$((count_ok + 1))
 else
-	echo "Test:Fail"
+	echo -e "Test:Fail\n"
 	count_fail=$((count_fail + 1))
 fi
 
-echo -e "\n"
-
 echo "Multiplication Test"
-echo -e "Multiplying $var1 and $var2\n\n"
+echo "Multiplying $var1 and $var2"
 yippy_test=$(${YIPPY_EXE} -e "* $var1 $var2")
 bash_test=$(expr ""$var1 "*" $var2"")
 
 if [[ $yippy_test -eq $bash_test ]]; then
-	echo "Test:OK"
+	echo -e "Test:OK\n"
 	count_ok=$((count_ok + 1))
 else
-	echo "Test:Fail"
+	echo -e "Test:Fail\n"
 	count_fail=$((count_fail + 1))
 fi
 
 echo "Divide Test"
-echo -e "Dividing $var1 and $var2\n\n"
+echo "Dividing $var1 and $var2"
 yippy_test=$(${YIPPY_EXE} -e "/ $var1 $var2")
-bash_test=$(awk -v var1=${var1} -v var2=${var2} 'BEGIN {print "%.6g",( var1 / var2 )}')
-result=$(awk -v var1=${bash_test} -v var2=${yippy_test} 'BEGIN {print ( var1 == var2 )}')
+bash_test=$(python3 -c "print(\"{:.6f}\".format($var1/$var2))")
+result=$(python3 -c "print(1) if $bash_test==$yippy_test else print(0)")
 
 if [[ $result -ne 0 ]]; then
-	echo "Test:OK"
+	echo -e "Test:OK\n"
 	count_ok=$((count_ok + 1))
 else
-	echo "Test:Fail"
+	echo -e "Test:Fail\n"
 	count_fail=$((count_fail + 1))
 fi
 
-echo -e "\n"
-
 echo "Modulus Test"
-echo -e "Mod $var1 and $var2\n\n"
+echo "Mod $var1 and $var2"
 yippy_test=$(${YIPPY_EXE} -e "% $var1 $var2")
 bash_test=$(expr ""$var1 "%" $var2"")
 
 if [[ $yippy_test -eq $bash_test ]]; then
-	echo "Test:OK"
+	echo -e "Test:OK\n"
 	count_ok=$((count_ok + 1))
 else
-	echo "Test:Fail"
+	echo -e "Test:Fail\n"
 	count_fail=$((count_fail + 1))
 fi
-
-echo -e "\n"
