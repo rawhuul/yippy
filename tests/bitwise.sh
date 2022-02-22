3bb5bf1233cef1e0b1f248ecd46e9687ce86bd58
Rahul <basicfunc@gmail.com>
2022-02-22 14:00:03 +0530

    Added test for unary operators

--- tests/bitwise.sh
+++ tests/bitwise.sh
@@ -74,19 +74,4 @@ else
 	count_fail=$((count_fail + 1))
 fi
 
-bit=$RANDOM
-
-echo "Bitwise NOT Test"
-echo "~$bit"
-yippy_test=$(${YIPPY_EXE} -e "~ $bit")
-py_test=$(python3 -c "print(~ $bit)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
 done
--- tests/unary.sh
+++ tests/unary.sh
@@ -1,82 +1,50 @@
-echo "*************Bitwise Operations TEST****************"
+echo "*************Unary Operations TEST****************"
 
 for i in {1..50};
 do
 
-var1=$(($RANDOM % 2))
-var2=$(($RANDOM % 2))
-
-echo "Bitwise OR Test"
-echo "$var1 | $var2"
-yippy_test=$(${YIPPY_EXE} -e "| $var1 $var2")
-py_test=$(python3 -c "print($var1 | $var2)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
-echo "Bitwise AND Test"
-echo "$var1 & $var2"
-yippy_test=$(${YIPPY_EXE} -e "& $var1 $var2")
-py_test=$(python3 -c "print($var1 & $var2)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
-var1=$(($RANDOM % 20))
-var2=$(($RANDOM % 20))
-
-echo "Bitwise << Test"
-echo "$var1 << $var2"
-yippy_test=$(${YIPPY_EXE} -e "<< $var1 $var2")
-py_test=$(python3 -c "print($var1 << $var2)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
-echo "Bitwise >> Test"
-echo "$var1 >> $var2"
-yippy_test=$(${YIPPY_EXE} -e ">> $var1 $var2")
-py_test=$(python3 -c "print($var1 >> $var2)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
-echo "Bitwise XOR Test"
-echo "$var1 ^ $var2"
-yippy_test=$(${YIPPY_EXE} -e "^ $var1 $var2")
-py_test=$(python3 -c "print($var1 ^ $var2)")
-
-if [[ $yippy_test -eq $py_test ]]; then
-	echo -e "Test:OK\n"
-	count_ok=$((count_ok + 1))
-else
-	echo -e "Test:Fail\n"
-	count_fail=$((count_fail + 1))
-fi
-
 bit=$RANDOM
 
-echo "Bitwise NOT Test"
+echo "Unary Plus Test"
+echo "+$bit"
+yippy_test=$(${YIPPY_EXE} -e "+ $bit")
+py_test=$(python3 -c "print(+ $bit)")
+
+if [[ $yippy_test -eq $py_test ]]; then
+	echo -e "Test:OK\n"
+	count_ok=$((count_ok + 1))
+else
+	echo -e "Test:Fail\n"
+	count_fail=$((count_fail + 1))
+fi
+
+echo "Unary Negation Test"
+echo "-$bit"
+yippy_test=$(${YIPPY_EXE} -e "- $bit")
+py_test=$(python3 -c "print(- $bit)")
+
+if [[ $yippy_test -eq $py_test ]]; then
+	echo -e "Test:OK\n"
+	count_ok=$((count_ok + 1))
+else
+	echo -e "Test:Fail\n"
+	count_fail=$((count_fail + 1))
+fi
+
+echo "Unary Negation Test"
+echo "!$bit"
+yippy_test=$(${YIPPY_EXE} -e "! $bit")
+py_test=$(python3 -c "print(int(not $bit))")
+
+if [[ $yippy_test -eq $py_test ]]; then
+	echo -e "Test:OK\n"
+	count_ok=$((count_ok + 1))
+else
+	echo -e "Test:Fail\n"
+	count_fail=$((count_fail + 1))
+fi
+
+echo "Unary One's Complement Test"
 echo "~$bit"
 yippy_test=$(${YIPPY_EXE} -e "~ $bit")
 py_test=$(python3 -c "print(~ $bit)")
