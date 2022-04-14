1d10f79213f7a179cc1bf08b0b64f8592f1676c9
Rahul <basicfunc@gmail.com>
2022-04-14 16:54:33 +0530

    Fixed  functiongit commit -m !

--- src/wasm.c
+++ src/wasm.c
@@ -9,28 +9,35 @@
 #include <string.h>
 #include <time.h>
 
+static char tmp[2048];
+
+#define CONCAT(string, fmt, ...)                                               \
+  sprintf(tmp, fmt, ##__VA_ARGS__);                                            \
+  strcat(string, tmp)
+
 void wa_print(value *v, char *str);
 
 static scope *global = NULL;
 
 void wa_print_expr(value *v, char open, char close, char *str) {
-  sprintf(str + strlen(str), "%c", open);
+  CONCAT(str, "%c", open);
+
   for (int i = 0; i < v->count; i++) {
 
     wa_print(v->cell[i], str);
 
     if (i != (v->count - 1)) {
-      strcat(str, " ");
+      CONCAT(str, " ");
     }
   }
-  sprintf(str + strlen(str), "%c", close);
+  CONCAT(str, "%c", close);
 }
 
 void wa_print_str(value *v, char *str) {
   char *tmp = malloc(strlen(v->string) + 1);
   strcpy(tmp, v->string);
   tmp = mpcf_escape(tmp);
-  sprintf(str + strlen(str), "\"%s\"", tmp);
+  CONCAT(str, "\"%s\"", tmp);
   free(tmp);
 }
 
@@ -38,29 +45,29 @@ void wa_print(value *v, char *str) {
   switch (v->type) {
   case NUMBER: {
     if (ifDouble(v->num) == 1) {
-      sprintf(str + strlen(str), "%Lf", v->num);
+      CONCAT(str, "%Lf", v->num);
     } else {
-      sprintf(str + strlen(str), "%ld", (long)v->num);
+      CONCAT(str, "%ld", (long)v->num);
     }
   } break;
   case FUNCTION: {
     if (v->func) {
-      sprintf(str + strlen(str), "<function>");
+      CONCAT(str, "<function>");
     } else {
-      strcat(str, "(lambda");
+      CONCAT(str, "(lambda");
       wa_print(v->formals, str);
-      strcat(str, " ");
+      CONCAT(str, " ");
       wa_print(v->body, str);
-      strcat(str, ")");
+      CONCAT(str, ")");
     }
   } break;
   case OK:
     break;
   case ERR:
-    sprintf(str + strlen(str), "Error: %s", v->error);
+    CONCAT(str, "Error: %s", v->error);
     break;
   case SYMBOL:
-    sprintf(str + strlen(str), "%s", v->symbol);
+    CONCAT(str, "%s", v->symbol);
     break;
   case STRING:
     wa_print_str(v, str);
@@ -76,10 +83,19 @@ void wa_print(value *v, char *str) {
 
 char *wa_println(value *v) {
   if (v->type != OK) {
-    char *res = malloc(4096);
+    char res[4096];
     wa_print(v, res);
-    strcat(res, "\n");
-    return res;
+    /* CONCAT(res, "\n"); */
+
+    char *string = malloc(strlen(res) + 1);
+
+    if (!string) {
+      return NULL;
+    } else {
+      strcpy(string, res);
+    }
+
+    return string;
   }
   return NULL;
 }
